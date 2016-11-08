02a676e706b2e032d9f6c6113020eec1ab43340d
chamirkazhiyev <chamirkazhiyev@greet-go.com>
2016-11-08 16:39:34 +0600

    Поменял дизайн

--- click.parent/click.client/www/app/view/ViewMainPage.js
+++ click.parent/click.client/www/app/view/ViewMainPage.js
@@ -1,6 +1,6 @@
 Ext.define('Click.view.ViewMainPage', {
   extend: 'Ext.Container',
-  xtype: 'viewMain',
+  xtype : 'viewMain',
 
   requires: [
     'Ext.Panel',
@@ -10,23 +10,23 @@ Ext.define('Click.view.ViewMainPage', {
     'Ext.Toolbar'
   ],
 
-  config: {
-    cls: 'cards',
+  config    : {
+    cls   : 'cards',
     layout: {
-      type: 'vbox',
+      type : 'vbox',
       align: 'stretch'
     },
 
-    items: [{
+    items    : [{
 
-      xtype: 'toolbar',
+      xtype : 'toolbar',
       docked: 'top',
-      title: 'Sliding menu',
-      items: [{
+      items : [{
         xtype: 'button',
+        text : 'menu',
 
-        handler: function(){
-          if(Ext.Viewport.getMenus().left.isHidden()) {
+        handler: function () {
+          if (Ext.Viewport.getMenus().left.isHidden()) {
             Ext.Viewport.showMenu('left');
           } else {
             Ext.Viewport.hideMenu('left');
@@ -35,174 +35,158 @@ Ext.define('Click.view.ViewMainPage', {
         }
       },
         {
-          xtype: 'button',
-          text: 'H',
+          xtype : 'button',
+          text  : 'H',
           action: 'openFooter',
           docked: 'right'
         }]
     },
       {
-      xtype: 'container',
-      layout: 'hbox',
-      flex: 17,
+        xtype : 'container',
+        layout: 'hbox',
+        flex  : 17,
 
-      items: [{
-        xtype: 'label',
-        style: 'font: normal 30px Neuron',
+        items: [{
+          xtype: 'label',
+          style: 'font: normal 30px courier',
 
-        html: 'Общий баланс'
 
-      }]
-    }, {
-      xtype: 'carousel',
-      direction: 'horizontal',
-      flex: 29,
-      defaults: {
-        xtype: 'carouselElement'
+        }]
+      }, {
+        xtype    : 'carousel',
+        direction: 'horizontal',
+        flex     : 29,
+        defaults : {
+          xtype: 'carouselElement'
+        },
+
+        items: [{
+          sum   : '234000.99',
+          number: '432145432656365',
+        }, {
+          sum: '1700000.23'
+        }, {
+          sum: '100000.99'
+        }, {
+          sum: '1111'
+        }]
+      }, {
+        xtype   : 'container',
+        layout  : 'hbox',
+        flex    : 14,
+        padding : '0 0.5em 0 0',
+        defaults: {
+          flex     : 1,
+          margin   : '0.5em 0 0 0.5em',
+          xtype    : 'button',
+          ui       : 'big-icon',
+          iconAlign: 'top',
+          iconMask : true
+        },
+
+        items: [{
+          text  : 'ОПЛАТА',
+          icon  : 'resources/icons/32x32/main/Icon32x32_Payment.png',
+          action: 'myPay'
+        }, {
+          text  : 'ПЕРЕВОДЫ',
+          icon  : 'resources/icons/32x32/main/Icon32x32_Transactions.png',
+          action: 'transfer'
+
+        }, {
+          text  : 'АВТОПЛАТЕЖ',
+          icon  : 'resources/icons/32x32/main/Icon32x32_MyCards.png',
+          action: 'autoPay'
+        }]
+      }, {
+        xtype   : 'container',
+        layout  : 'hbox',
+        flex    : 18,
+        padding : '0 0.5em 0 0',
+        defaults: {
+          margin   : '0.5em 0 0 0.5em',
+          xtype    : 'button',
+          ui       : 'big-icon',
+          iconAlign: 'top'
+        },
+
+        items: [{
+          text  : 'МОИ ФИНАНСЫ',
+          icon  : 'resources/icons/32x32/main/Icon32x32_MyFinances.png',
+          action: 'myFinance'
+
+        }, {
+          text: 'ОТЧЁТЫ',
+          icon: 'resources/icons/32x32/main/Icon32x32_Reports.png',
+          action: 'report'
+        }]
+      }, {
+        xtype   : 'container',
+        layout: 'hbox',
+        flex  : 20,
+        padding: '0 0.5em 0 0',
+        defaults: {
+          flex     : 1,
+          margin: '0.5em 0 0 0.5em',
+          xtype : 'button',
+          ui    : 'big-icon',
+          iconAlign: 'top'
+        }
+
+        /*items: [{
+         icon: 'resources/icons/32x32/logos/Icon32x32_logo1.png'
+         }, {
+         icon: 'resources/icons/32x32/logos/Icon32x32_logo2.png'
+         }, {
+         icon: 'resources/icons/32x32/logos/Icon32x32_logo3.png'
+         }, {
+         icon: 'resources/icons/32x32/logos/Icon32x32_logo1.png'
+         }, {
+         icon: 'resources/icons/32x32/logos/Icon32x32_logo2.png'
+         }, {
+         icon: 'resources/icons/32x32/logos/Icon32x32_logo3.png'
+         }]*/
+      }, {
+        xtype   : 'container',
+        layout: 'hbox',
+        flex  : 5,
+        defaults: {
+          flex  : 1,
+          margin: '0.5em',
+          xtype : 'label'
+        }
       },
-
-      items: [{
-        sum: '234000.99',
-        number: '432145432656365',
-      }, {
-        sum: '1700000.23'
-      }, {
-        sum: '100000.99'
-      }, {
-        sum: '1111'
-      }]
-    }, {
-      xtype: 'container',
-      layout: 'hbox',
-      flex: 14,
-      padding: '0 0.5em 0 0',
-      defaults: {
-        flex: 1,
-        margin: '0.5em 0 0 0.5em',
-        xtype: 'button',
-        ui: 'big-icon',
-        iconAlign: 'top',
-        iconMask: true
-      },
-
-      items: [{
-        text: 'ОПЛАТА',
-        icon: 'resources/icons/32x32/main/Icon32x32_Payment.png',
-        action: 'myPay'
-      }, {
-        text: 'ПЕРЕВОДЫ',
-        icon: 'resources/icons/32x32/main/Icon32x32_Transactions.png',
-        action: 'transfer'
-
-      }, {
-        text: 'АВТОПЛАТЕЖ',
-        icon: 'resources/icons/32x32/main/Icon32x32_MyCards.png',
-        action: 'autoPay'
-      }]
-    }, {
-      xtype: 'container',
-      layout: 'hbox',
-      flex: 18,
-      padding: '0 0.5em 0 0',
-      defaults: {
-        flex: 1,
-        margin: '0.5em 0 0 0.5em',
-        xtype: 'button',
-        ui: 'big-icon',
-        iconAlign: 'top'
-      },
-
-      items: [{
-        text: 'МОИ ФИНАНСЫ',
-        icon: 'resources/icons/32x32/main/Icon32x32_MyFinances.png',
-        action: 'myFinance'
-
-      }, {
-        text: 'ОТЧЁТЫ',
-        icon: 'resources/icons/32x32/main/Icon32x32_Reports.png',
-        action: 'report'
-      }]
-    }, {
-      xtype: 'container',
-      layout: 'hbox',
-      flex: 20,
-      padding: '0 0.5em 0 0',
-      defaults: {
-        flex: 1,
-        margin: '0.5em 0 0 0.5em',
-        xtype: 'button',
-        ui: 'big-icon',
-        iconAlign: 'top'
-      }
-
-      /*items: [{
-        icon: 'resources/icons/32x32/logos/Icon32x32_logo1.png'
-      }, {
-        icon: 'resources/icons/32x32/logos/Icon32x32_logo2.png'
-      }, {
-        icon: 'resources/icons/32x32/logos/Icon32x32_logo3.png'
-      }, {
-        icon: 'resources/icons/32x32/logos/Icon32x32_logo1.png'
-      }, {
-        icon: 'resources/icons/32x32/logos/Icon32x32_logo2.png'
-      }, {
-        icon: 'resources/icons/32x32/logos/Icon32x32_logo3.png'
-      }]*/
-    }, {
-      xtype: 'container',
-      layout: 'hbox',
-      flex: 5,
-      defaults: {
-        flex: 1,
-        margin: '0.5em',
-        xtype: 'label'
-      }
-    },
       {
-        xtype: 'toolbar',
+        xtype : 'toolbar',
         docked: 'bottom',
-        items:[
-          {
-            xtype: 'spacer'
-          },
-          {
-            xtype: 'button',
-            text: 'Контакты+',
-            handler: function () {
-              Ext.select('#listId').show();
-            }
-          },
+        title : 'Контакты ^',
 
-          {
-            xtype: 'spacer'
-          }
-          ]
       }],
-    listeners:{
-      initialize: function(){
-        Ext.Viewport.setMenu(this.createMenu(),{
-          side: 'left',
+    listeners: {
+      initialize: function () {
+        Ext.Viewport.setMenu(this.createMenu(), {
+          side  : 'left',
           reveal: true
         });
       }
     }
   },
 //Sliding menu Here
-  createMenu: function(){
+  createMenu: function () {
     var items = [
       {
-        xtype: 'button',
-        text: 'button1',
+        xtype  : 'button',
+        text : 'button1',
         iconCls: 'user',
-        class: 'menu-button'
+        class  : 'menu-button'
       }
     ];
     return Ext.create('Ext.Menu', {
-      style: 'padding: 0',
-      id: 'menu',
+      style     : 'padding: 0',
+      id   : 'menu',
       width: 300,
       scrollable: 'vertical',
-      items: items
+      items     : items
     });
   }
 });
