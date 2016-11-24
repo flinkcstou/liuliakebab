d529c47dc8d1f87e7af50079cbc6d5068b6283e7
ekolpakov <ekolpakov@greet-go.com>
2016-11-24 11:36:27 +0600

    Начало реализации сборки

---
+++ click.parent/click.client/test_src/kz/greetgo/click/client/PrepareCordovaWWW.java
@@ -0,0 +1,8 @@
+package kz.greetgo.click.client;
+
+public class PrepareCordovaWWW {
+  public static void main(String[] args) {
+    String indexName = args[0];
+    System.out.println("indexName = " + indexName);
+  }
+}
