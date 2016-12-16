2cbf6d7de2d632ca543522024f47a460dfbada52
chamirkazhiyev <chamirkazhiyev@greet-go.com>
2016-12-16 21:09:27 +0600

    revert authorization and other

--- click.parent/click.client/www/stand/plugins/plugins_fake.js
+++ click.parent/click.client/www/stand/plugins/plugins_fake.js
@@ -1,5 +1,5 @@
 window.device_fake = {};
-window.device_fake.name = 'iPhone 9';
+window.device_fake.model = 'iPhone 9';
 window.device_fake.uuid = '232323232323';
 window.device_fake.platform = 'IOS';
 window.device_fake.info = 'iPhone OS 9_3_5 Version/9.0 232323232323';
