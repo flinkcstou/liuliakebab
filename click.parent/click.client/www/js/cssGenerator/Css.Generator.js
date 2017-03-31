391ca3fda108f4334c0975c770ae5944c8300002
chamirkazhiyev <chamirkazhiyev@greet-go.com>
2017-03-31 13:59:58 +0600

    gradient changed CHECK

--- click.parent/click.client/www/js/cssGenerator/Css.Generator.js
+++ click.parent/click.client/www/js/cssGenerator/Css.Generator.js
@@ -55,9 +55,9 @@ var css = {
     //"background-image: -moz-linear-gradient(rgb(1,124,227) 0%, rgb(21,181,243) 50%, rgb(21,181,243) 100%);" +
     //"background-image: -webkit-linear-gradient(rgb(1,124,227) 0%, rgb(21,181,243) 50%, rgb(21,181,243) 100%);" +
     //"background-image: -ms-linear-gradient(rgb(1,124,227) 0%, rgb(21,181,243) 50%, rgb(21,181,243) 100%);" +
-  "background: -moz-linear-gradient(rgb(0,168,241),rgb(73,212,255)60%);" +
-  "background: -webkit-linear-gradient(rgb(0,168,241),rgb(73,212,255)60%);" +
-  "background: -ms-linear-gradient(rgb(0,168,241),rgb(73,212,255)60%);" +
+  "background: -moz-linear-gradient(rgb(0,168,241),rgb(73,212,255)80%);" +
+  "background: -webkit-linear-gradient(rgb(0,168,241),rgb(73,212,255)80%);" +
+  "background: -ms-linear-gradient(rgb(0,168,241),rgb(73,212,255)80%);" +
   "font-family: SFUIDisplay-Light;" +
   "margin: 0;" +
   "overflow: hidden;" +
