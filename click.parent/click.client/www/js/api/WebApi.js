71a38a7c01afff39c231cca5fd2be15cfbc93d89
chamirkazhiyev <chamirkazhiyev@greet-go.com>
2017-06-02 10:08:45 +0600

    onbackkey() down deleted from webApi

--- click.parent/click.client/www/js/api/WebApi.js
+++ click.parent/click.client/www/js/api/WebApi.js
@@ -269,12 +269,12 @@ window.api.call = function (params) {
   }
 
   if (modeOfApp.onlineMode && (method == "get.additional.information" || method == "get.payment" || method == "app.login"
-    || method == "device.register.request" || method == "device.register.request" || method == "device.register.confirm"))
+    || method == "device.register.request" || method == "registration" || method == "device.register.confirm"))
     setTimeout(function () {
       if (device.platform != 'BrowserStand')
         if (window.api.spinnerOn) {
           SpinnerPlugin.activityStop();
-          onBackKeyDown();
+          //onBackKeyDown();
           showAlertComponent("Сервис временно недоступен");
 
         }
