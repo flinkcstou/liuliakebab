02509974c468e0f2cd0447736c702df93c954aff
bduisenbayeva <bduisenbayeva@greet-go.com>
2017-09-15 17:32:48 +0600

    pickContact plugin - old version returned

--- click.parent/click.client/test_src/kz/greetgo/click/client/AbstractCompileCordova.java
+++ click.parent/click.client/test_src/kz/greetgo/click/client/AbstractCompileCordova.java
@@ -19,8 +19,9 @@ public abstract class AbstractCompileCordova {
 
   protected void addPluginPickcontact() throws IOException, InterruptedException {
 //    clickClient.cmd("npm install");
-    clickClient.cd("cordova").cmd("cordova plugin add https://github.com/kolwit/com.kolwit.pickcontact.git");
+    clickClient.cd("cordova").cmd("cordova plugin add https://github.com/kolwit/com.kolwit.pickcontact.git#64eafca2b31e4294ff26900291937eb9640067f9");
     clickClient.cd("cordova").cmd("cordova plugin add https://github.com/Telerik-Verified-Plugins/Keyboard");
     clickClient.cmd("cp www/index-stand-demo.html cordova/www");
+    //a14a427f6ec8a75cd4ffca75afc8d69c3619331e
   }
 }
