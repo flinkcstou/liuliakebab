543b0a0fb6fa525bb38f7c87ca3b87c1e874296f
bduisenbayeva <bduisenbayeva@greet-go.com>
2017-09-20 10:08:26 +0600

    servicePageAmountInput - top from px to %

--- click.parent/click.client/www/js/cssGenerator/Css.Generator.js
+++ click.parent/click.client/www/js/cssGenerator/Css.Generator.js
@@ -3965,7 +3965,7 @@ var css = {
   "border: none;" +
   "outline: none;" +
   "position: relative;" +
-  "top:" + 26 * widthK + "px; " +
+  "top: 30%; " +
   "width: 88%;" +
   "font-size:" + 54 * widthK + "px; " +
   "font-family: 'SFUIDisplay-Light';" +
