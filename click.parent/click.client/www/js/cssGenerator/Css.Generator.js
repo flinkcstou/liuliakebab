087cf47291ab4d8bebcfa2992ea93f9bcf63b6f0
bduisenbayeva <bduisenbayeva@greet-go.com>
2017-10-12 15:47:42 +0600

    AddCard view - boxone style correction

--- click.parent/click.client/www/js/cssGenerator/Css.Generator.js
+++ click.parent/click.client/www/js/cssGenerator/Css.Generator.js
@@ -10654,7 +10654,7 @@ var css = {
   "background-color: transparent;" +
   "border: none;" +
   "outline: none;" +
-  "font-size:" + 35 * widthK + "px;" +
+  "font-size:" + 34 * widthK + "px;" +
   //"letter-spacing:1px;" +
   "word-spacing:" + 25 * widthK + "px;" +
   "}",
