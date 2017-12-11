7933ba0aadcf27f91c04842800c7b75358389a6b
damangeldy <damangeldy@greet-go.com>
2017-12-11 17:04:17 +0600

    Bugfixes for release: alert on iOS 11 and text unselect everywhere

--- click.parent/click.client/www/js/api/WebApi.js
+++ click.parent/click.client/www/js/api/WebApi.js
@@ -37,7 +37,9 @@ window.api.call = function (params, timeout) {
     if (window.api.socket.readyState === 3) {
       console.log("Connection in state 3, opening new connection for next requests");
       window.isConnected = false;
-      window.api.init();
+      if (navigator.connection.type !== Connection.NONE) {
+        window.api.init();
+      }
       stateCheckerCleared = true;
       clearInterval(stateChecker);
     }
@@ -92,7 +94,9 @@ window.api.initSocket = function () {
     console.log('Connection is closed');
     console.log(event);
     window.isConnected = false;
-    window.api.init();
+    if (navigator.connection.type !== Connection.NONE) {
+      window.api.init();
+    }
   };
 
   var me = this;
