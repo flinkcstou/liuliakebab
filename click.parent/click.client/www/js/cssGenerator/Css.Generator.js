1eccf965f1e9444c7f8a26dce0aba86f4426c835
bduisenbayeva <bduisenbayeva@greet-go.com>
2018-01-05 13:28:57 +0600

    indoor service list - scroll

--- click.parent/click.client/www/js/cssGenerator/Css.Generator.js
+++ click.parent/click.client/www/js/cssGenerator/Css.Generator.js
@@ -15865,7 +15865,7 @@ var css = {
   "width: 100%; " +
   "top:15%; " +
   "bottom:0; " +
-  "overflow-y: auto; " +
+  "overflow-y: scroll; " +
   "overflow-x: hidden; " +
   "-webkit-overflow-scrolling: touch; " +
   "}",
