67ec809391231748d3ea70ab1fa21f8df65a33e8
bduisenbayeva <bduisenbayeva@greet-go.com>
2018-01-17 11:51:27 +0600

    InPlacePayService - moved paging to onscroll event for ios

--- click.parent/click.client/www/js/cssGenerator/Css.Generator.js
+++ click.parent/click.client/www/js/cssGenerator/Css.Generator.js
@@ -15995,11 +15995,11 @@ var css = {
   "-webkit-overflow-scrolling: touch; " +
   "}",
 
-  inPlacePayServiceInnerContainerScroll: ".inplace-pay-service-inner-container::-webkit-scrollbar" +
-  "{" +
-  "width: 20px; " +
-  "z-index: 99999999999;" +
-  "}",
+  // inPlacePayServiceInnerContainerScroll: ".inplace-pay-service-inner-container::-webkit-scrollbar" +
+  // "{" +
+  // "width: 20px; " +
+  // "z-index: 99999999999;" +
+  // "}",
 
 
   inPlacePayBlockContainer: ".inplace-pay-block-containter" +
