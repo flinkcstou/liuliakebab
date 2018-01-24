11680256c10cc54ca4936c7de317a8d34103354e
bduisenbayeva <bduisenbayeva@greet-go.com>
2018-01-24 15:55:25 +0600

    writeLog function ready - without ID

--- click.parent/click.client/www/js/globalObjects/global.Objects.js
+++ click.parent/click.client/www/js/globalObjects/global.Objects.js
@@ -1658,3 +1658,105 @@ window.sendToLog = function (data) {
   }
   // window.api.send(params);
 };
+
+var fileData = [], log;
+
+window.writeLog = function (logToSave) {
+
+  log = logToSave;
+
+  readFromFile('log.txt', function (data) {
+    if (data) {
+      console.log("READ FILE BEFORE = ", JSON.parse(data));
+      fileData = JSON.parse(data);
+      writeToFile('log.txt', log);
+    }
+  });
+
+}
+
+
+function writeToFile(fileName, log) {
+
+  console.log("log=", log);
+  fileData.push(log);
+  console.log("DATA TO WRITE = ", fileData);
+
+  var data = JSON.stringify(fileData, null, '\t');
+  window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+    directoryEntry.getFile(fileName, {create: true}, function (fileEntry) {
+      fileEntry.createWriter(function (fileWriter) {
+        fileWriter.onwriteend = function (e) {
+          // for real-world usage, you might consider passing a success callback
+          console.log('Write of file "' + fileName + '"" completed.');
+          readFromFile('log.txt', function (data) {
+            console.log("RESULT OF READ FILE = ", data);
+          });
+        };
+
+        fileWriter.onerror = function (e) {
+          // you could hook this up with our global error handler, or pass in an error callback
+          console.log('Write failed: ' + e.toString());
+        };
+
+        var blob = new Blob([data], {type: 'text/plain'});
+        // try {
+        //   fileWriter.seek(fileWriter.length);
+        // }
+        // catch (e) {
+        //   console.log("file doesn't exist!");
+        // }
+        fileWriter.write(blob);
+      }, errorHandler.bind(null, fileName));
+    }, errorHandler.bind(null, fileName));
+  }, errorHandler.bind(null, fileName));
+}
+
+
+function readFromFile(fileName, cb) {
+  var pathToFile = cordova.file.dataDirectory + fileName;
+  window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
+    fileEntry.file(function (file) {
+      var reader = new FileReader();
+
+      reader.onloadend = function (e) {
+        cb(this.result);
+      };
+
+      reader.readAsText(file);
+    }, errorHandler.bind(null, fileName));
+  }, errorHandler.bind(null, fileName));
+}
+
+
+var errorHandler = function (fileName, e) {
+  var msg = '';
+
+  switch (e.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'Storage quota exceeded';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'File not found';
+      writeToFile('log.txt', log);
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'Security error';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'Invalid modification';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'Invalid state';
+      break;
+    default:
+      msg = 'Unknown error';
+      break;
+  }
+
+  console.log('Error (' + fileName + '): ' + msg);
+}
+
+
+
+
