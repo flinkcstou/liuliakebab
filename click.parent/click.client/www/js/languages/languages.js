dfa6d304adb4553753139d7e4dfe3fb3e32b8662
bduisenbayeva <bduisenbayeva@greet-go.com>
2018-06-22 09:56:26 +0600

    version 5.3.4 - 21 июня

--- click.parent/click.client/www/js/languages/languages.js
+++ click.parent/click.client/www/js/languages/languages.js
@@ -359,7 +359,7 @@ var RU_LANGUAGES =
   "ViewSettingsAboutProgramTitleName": "О ПРОГРАММЕ",
   "ViewSettingsAboutProgramVersionTitleNamePartOne": "Версия 5.3.3",
   "ViewSettingsAboutProgramVersionTitleNamePartOneClick": "CLICK Uzbekistan",
-  "ViewSettingsAboutProgramVersionTitleNamePartTwo": "22 мая, 2018",
+  "ViewSettingsAboutProgramVersionTitleNamePartTwo": "21 июня, 2018",
   "ViewSettingsAboutProgramUserAgreementTitleName": "Пользовательское соглашение",
   "ViewSettingsAboutProgramRankTitleName": "Оцените нас в Google Play",
 
